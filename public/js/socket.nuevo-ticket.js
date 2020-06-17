f3258877fe87443b72f92b9d093482233d6ffe28
Ramon <ju4nrsuarez@gmail.com>
2020-06-17 01:15:29 -0300

    Produccion

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,33 @@
+
+
+// Comando para establecer la conexion
+var socket = io();
+
+var label = $('#lblNuevoTicket');
+
+
+socket.on('connect', function(){
+    console.log('Conectando al servidor');
+});
+
+socket.on('disconnect', function(){
+    console.log('Desconectado  del servidor');
+});
+
+socket.on('estadoActual', function(resp){
+    
+    console.log(resp);
+    label.text( resp.actual);
+});
+
+
+
+
+$('button').on('click', function(){
+
+    socket.emit('siguienteTicket', null, function(siguienteTicket){
+
+        label.text(siguienteTicket);
+
+    });
+})
